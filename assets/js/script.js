e449a3c73cadb3632fe65d078b908faaaa874ca7
lizatamara <lizaa1609@gmail.com>
2024-02-14 00:32:18 -0300

    agrego tooltip y alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// smooth scroll
+
 $(document).ready(function(){
     $("a").on('click', function(event) {
     if (this.hash !== "") {
@@ -12,4 +14,15 @@ $(document).ready(function(){
     }
     });
    });
+
+//inicializar tooltips
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+// alert formulario
+
+$('#alert-form').on('click', function () {
+    alert('Mensaje enviado correctamente, nos contactaremos a la brevedad')
+});
    
